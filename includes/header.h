8949a239a15d1538f2c4b9c8ff9f2288e3bd2d3d
Roos Cocolien <r.steigenga@gmail.com>
2019-05-29 13:02:02 +0200

    Add files via upload
    includes

---
+++ includes/header.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   header.h                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rsteigen <rsteigen@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/05/25 13:25:23 by rsteigen       #+#    #+#                */
+/*   Updated: 2019/05/25 13:28:29 by rsteigen      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include "libft/libft.h"
+# include <stdargh.h>
+
+int		ft_printf(const char * restrict format, ...);
+
+#endif
